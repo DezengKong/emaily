c36b7d214534c6ef327096bca80da961e03dd781
KzzD <davicekong@outlook.com>
2023-05-28 22:26:12 -0400

    without the front client

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
-const keys = require('./config/keys')
 const bodyParser = require('body-parser')
+const keys = require('./config/keys')
 require('./models/User')
+require('./models/Survey')
 require('./services/passport')
-
 mongoose.connect(keys.mongoURI)
 
 const app = express()
@@ -23,12 +23,14 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+require('./routes/surveyRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assests
-  // like our main.js file, or main.css file
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static('client/build'))
-  // Express wil serve up the index.html file
+
+  // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path')
   app.get('*', (req, res) => {
